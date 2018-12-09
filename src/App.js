1ddc0d13eee0bd600abb9688d8cc5da59b7a64a1
Grace Hartzell <gracehartzell@utexas.edu>
2018-12-09 12:13:55 -0600

    Switch from function to class comp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-const Pet = (props) => { 
+const Pet = (props) => {                    // function component
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
     React.createElement("h2", {}, props.animal),
@@ -6,25 +6,27 @@ const Pet = (props) => {
   ]);
 }
 
-const App = () => { // render function for the component
-  return React.createElement("div", {}, [   // {} attributes (e.g. an id tag); 3rd is child attributes
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Lieu",
-      animal: "dog",
-      breed: "Pyrenees-Husky"
-    }),
-    React.createElement(Pet, {
-      name: "Loki",
-      animal: "dog",
-      breed: "Pittiesh"
-    }),
-    React.createElement(Pet, {
-      name: "Captain",
-      animal: "cat",
-      breed: "Korat"
-    })
-  ]);
+class App extends React.Component {
+  render() {
+    return React.createElement("div", {}, [   // {} attributes (e.g. an id tag); 3rd is child attributes
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {
+        name: "Lieu",
+        animal: "dog",
+        breed: "Pyrenees-Husky"
+      }),
+      React.createElement(Pet, {
+        name: "Loki",
+        animal: "dog",
+        breed: "Pittiesh"
+      }),
+      React.createElement(Pet, {
+        name: "Captain",
+        animal: "cat",
+        breed: "Korat"
+      })
+    ]);
+  }
 }
 
 // do something with the component:
