4952c3d7aa52deac513bfa44b211cd4a23b14837
Grace Hartzell <gracehartzell@utexas.edu>
2018-12-09 12:10:04 -0600

    Little more flexible

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-const Pet = () => {
+const Pet = (props) => { 
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Lieu"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Pyrenees-Husky")
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
   ]);
 }
 
 const App = () => { // render function for the component
   return React.createElement("div", {}, [   // {} attributes (e.g. an id tag); 3rd is child attributes
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet)
+    React.createElement(Pet, {
+      name: "Lieu",
+      animal: "dog",
+      breed: "Pyrenees-Husky"
+    }),
+    React.createElement(Pet, {
+      name: "Loki",
+      animal: "dog",
+      breed: "Pittiesh"
+    }),
+    React.createElement(Pet, {
+      name: "Captain",
+      animal: "cat",
+      breed: "Korat"
+    })
   ]);
 }
 
