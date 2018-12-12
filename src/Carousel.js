873fbff23043d81dd1fe67e041e7a1a58772f022
Grace Hartzell <gracehartzell@utexas.edu>
2018-12-12 10:21:22 -0600

    Added functionality to Carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -15,6 +15,27 @@ class Carousel extends React.Component {
 
     return { photos }; // same as doing photos: photos
   }
+
+  // arrow function doesn't create a new context when called; just refers to the lexical scope it's in
+  // if didn't do an arrow for handleIndexClick, would have to do go into constructor and do the following:
+  /*
+  constructor(props) {
+    super(props);
+    this.handleIndexClick = this.handleIndexClick.bind(this)
+  }
+
+    would have to do this for anything that'd be an event listener for calling this.setState, etc. would have to add it to that
+    binding the context
+
+    so handleIndexClick would refer to Carousel 
+  */
+  handleIndexClick = event => {
+    this.setState({
+      // called as event handler;
+      active: +event.target.dataset.index // (+) takes string and coerces into number because active is defined as a number
+    });
+  };
+
   render() {
     const { photos, active } = this.state;
 
@@ -28,8 +49,12 @@ class Carousel extends React.Component {
         <img src={hero} alt="animal" />
         <div className="carousel-smaller">
           {photos.map((photo, index) => (
+            /* doing disable because technically these pictures should be buttons */
+            /* eslint-disable-next-line */
             <img
+              onClick={this.handleIndexClick}
               key={photo.value}
+              data-index={index}
               src={photo.value}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
