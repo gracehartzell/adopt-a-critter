9e321f05625ab1d7ff4fa506d6ea424f68bcb050
Grace Hartzell <gracehartzell@utexas.edu>
2018-12-13 15:56:10 -0600

    Making form work

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -11,56 +11,64 @@ Context provides more app state related things (like breed searching for)
 4. Change `this`/`this.state` to `context` since the information is coming from context now
  */
 class SearchParams extends React.Component {
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.props.search();
+  };
   render() {
     return (
       <Consumer>
         {/* context is the state from App; can now reference context.breed and it will be the state from App */}
         {context => (
           <div className="search-params">
-            <label htmlFor="location">
-              Location
-              <input
-                onChange={context.handleLocationChange}
-                type="text"
-                id="location"
-                value={context.location}
-                placeholder="Location"
-              />
-            </label>
-            <label htmlFor="animal">
-              Animal Type
-              <select
-                id="animal"
-                value={context.animal}
-                onChange={context.handleAnimalChange}
-                onBlur={context.handleAnimalChange}
-              >
-                <option />
-                {ANIMALS.map(animal => (    // ANIMALS stays the same since it's coming from the modules
-                  <option key={animal} value={animal}>
-                    {animal}
-                  </option>
-                ))}
-              </select>
-            </label>
-            <label htmlFor="breed">
-              Breed
-              <select
-                id="breed"
-                value={context.breed}
-                onChange={context.handleBreedChange}
-                onBlur={context.handleBreedChange}
-                disabled={!context.breeds.length}
-              >
-                <option />
-                {context.breeds.map(breed => (
-                  <option key={breed} value={breed}>
-                    {breed}
-                  </option>
-                ))}
-              </select>
-            </label>
-            <button>Submit</button>
+            <form onSubmit={this.handleFormSubmit}>
+              <label htmlFor="location">
+                Location
+                <input
+                  onChange={context.handleLocationChange}
+                  type="text"
+                  id="location"
+                  value={context.location}
+                  placeholder="Location"
+                />
+              </label>
+              <label htmlFor="animal">
+                Animal Type
+                <select
+                  id="animal"
+                  value={context.animal}
+                  onChange={context.handleAnimalChange}
+                  onBlur={context.handleAnimalChange}
+                >
+                  <option />
+                  {ANIMALS.map((
+                    animal // ANIMALS stays the same since it's coming from the modules
+                  ) => (
+                    <option key={animal} value={animal}>
+                      {animal}
+                    </option>
+                  ))}
+                </select>
+              </label>
+              <label htmlFor="breed">
+                Breed
+                <select
+                  id="breed"
+                  value={context.breed}
+                  onChange={context.handleBreedChange}
+                  onBlur={context.handleBreedChange}
+                  disabled={!context.breeds.length}
+                >
+                  <option />
+                  {context.breeds.map(breed => (
+                    <option key={breed} value={breed}>
+                      {breed}
+                    </option>
+                  ))}
+                </select>
+              </label>
+              <button>Submit</button>
+            </form>
           </div>
         )}
       </Consumer>
